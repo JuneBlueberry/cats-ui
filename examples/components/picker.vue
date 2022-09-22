91bd2bdca2803a7bdb98703c1f88819e7b42345b
JuneBlueberry <812391718@qq.com>
2022-09-22 19:29:28 +0800

    更新 picker

--- examples/components/picker.vue
+++ examples/components/picker.vue
@@ -4,7 +4,7 @@
       <h1 class="demo-title">Picker</h1>
       <p class="demo-desc">选择器</p>
     </div>
-    <cats-picker></cats-picker>
+    <cats-picker :actions="actions1"></cats-picker>
   </div>
 </template>
 <script lang="ts">
@@ -14,7 +14,38 @@ export default {
     CatsPicker,
   },
   setup() {
-    return {};
+    const actions1 = [
+      {
+        title: "111",
+      },
+      {
+        title: "222",
+      },
+      {
+        title: "333",
+      },
+      {
+        title: "444",
+      },
+      {
+        title: "555",
+      },
+      {
+        title: "666",
+      },
+      {
+        title: "777",
+      },
+      {
+        title: "888",
+      },
+      {
+        title: "999",
+      },
+    ];
+    return {
+      actions1,
+    };
   },
 };
 </script>
--- packages/components/src/picker/picker.vue
+++ packages/components/src/picker/picker.vue
@@ -1,20 +1,140 @@
 <template>
   <div class="cats-picker">
-    <div class="cats-picker__header"></div>
-    <div class="cats-picker__wrapper"></div>
+    <div class="cats-picker__header">
+      <span class="cats-picker__header--btn cats-picker__header--btn--cancel"
+        >取消</span
+      >
+      <span class="cats-picker__header--btn cats-picker__header--btn--confirm"
+        >确定</span
+      >
+    </div>
+    <div class="cats-picker__body">
+      <div class="cats-picker__group">
+        <div class="cats-picker__mask"></div>
+        <div
+          class="cats-picker__content"
+          :style="style"
+          @touchstart="start($event)"
+          @touchmove="move($event)"
+          @touchend="end($event)"
+        >
+          <div
+            class="cats-picker__item"
+            v-for="(item, index) in items"
+            :key="index"
+            :style="item.style"
+          >
+            {{ item.title }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import "./style/index.scss";
-import { defineComponent, computed } from "vue";
+import { defineComponent, computed, ref } from "vue";
 import { createNamespace } from "../utils";
 import { pickerProps } from "./types";
 
 const [name] = createNamespace("picker");
 
+type Pos = {
+  x: number;
+  y: number;
+  time?: Number | any;
+};
+
 export default defineComponent({
   name,
+  props: pickerProps,
+  emits: ["start", "move", "end"],
+  setup(props, { emit }) {
+    let startPos: Pos;
+    let endPos: Pos;
+    const count = 7;
+    const middle = count % 2 == 0 ? count / 2 : (count + 1) / 2;
+    const height = 42;
+    const init = ref(0);
+    let current = 0;
+    const style = ref({
+      transform: "",
+      transition: "",
+    });
+
+    const items = computed(() => {
+      console.log(props.actions);
+
+      return props.actions.map((x, index) => {
+        const _style: any = {};
+        if (index <= count) {
+          _style.transform = `rotateX(${15 * (middle - index - 1)}deg)`;
+        }
+        return {
+          ...x,
+          style: _style,
+        };
+      });
+    });
+
+    const start = (event: TouchEvent) => {
+      const touch = event.targetTouches[0];
+      startPos = {
+        x: touch.pageX,
+        y: touch.pageY,
+        time: +new Date(),
+      };
+      emit("start", event);
+    };
+
+    const move = (event) => {
+      if (event.targetTouches.length > 1 || (event.scale && event.scale !== 1))
+        return;
+      const touch = event.targetTouches[0];
+      endPos = {
+        x: touch.pageX - startPos.x,
+        y: touch.pageY - startPos.y,
+      };
+      //阻止触摸事件的默认行为，即阻止滚屏
+      event.preventDefault();
+
+      change(false, endPos);
+    };
+
+    const end = (event: TouchEvent) => {
+      emit("end", event);
+
+      change(true, endPos);
+    };
+
+    const change = (isEnd: Boolean, endPos: Pos) => {
+      let distance = endPos.y;
+      if (isEnd) {
+        current += Math.round(endPos.y / 42);
+        distance = current * 42;
+
+        style.value = {
+          transform: `translate3d(0px, ${distance}px, 0px)`,
+          transition: "all 0.2s ease 0.2s",
+        };
+      } else {
+        distance = current * 42 + endPos.y;
+        style.value = {
+          transform: `translate3d(0px, ${distance}px, 0px)`,
+          transition: "all 0s ease 0s",
+        };
+      }
+    };
+
+    return {
+      style,
+      items,
+      start,
+      move,
+      end,
+    };
+  },
 });
 </script>
 
